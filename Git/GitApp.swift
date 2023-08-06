fa95e7c391c147b39dfef68796fd5ec18b84b0ea
notjorje <georgelupu2333@gmail.com>
2023-08-06 18:26:52 +0300

    Initial Commit

---
+++ Git/GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitApp.swift
+//  Git
+//
+//  Created by Lupu George on 06.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
